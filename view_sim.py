51a48ff432a3923ae8daca652ed45aa28179d2f5
Michael Naguib <1michael.naguib@gmail.com>
2019-08-20 15:12:31 -0500

    TL;DR QuickFix

--- view_sim.py
+++ view_sim.py
@@ -6,7 +6,6 @@
 # regular python myFile.py works each time! 
 
 # #imports
-print("Getting Imports")
 from tkinter import *
 from BoidSwarm import BoidSwarm
 from Vector import Vector
@@ -19,13 +18,11 @@ import pickle
 import open3d
 import time
 
-print("Finished Imports ...  Reading Simulation Data")
 #Settings
 timeDelay = 0.02 # 0.5=half second ... used to space out each frame in the playback
-loop = True
+
 #Read the file
 positionFrameHistory = pickle.load(open("position.dat","rb"))
-print("Finished Reading Simulation Data")
 
 #Setup 3D views
 pcd = open3d.PointCloud()
@@ -39,23 +36,13 @@ pcd.colors = open3d.Vector3dVector([[0,0,0] for i in range(0,len(positionFrameHi
 
 render_option = vis.get_render_option()
 render_option.point_size = 5#0.01
-print("Bootstraping Simulation Data")
+
 #precompute
 for i in range(1,len(positionFrameHistory)):
     positionFrameHistory[i]=open3d.Vector3dVector(positionFrameHistory[i])
 #render_option.point_color_option = open3d.visualization.PointColorOption.Normal
 to_reset_view_point = True
-
-print("Starting Playback")
-i=0
-while True: 
-    if i < len(positionFrameHistory)-1:
-        i+=1
-    elif not loop:
-        break
-    else:
-        print("Restarting Playback")
-        i=0  
+for i in range(1,len(positionFrameHistory)):    
     pcd.points = positionFrameHistory[i]
     vis.update_geometry()
     if to_reset_view_point:
