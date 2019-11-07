6ed77e5088082a8f78b17f433fbd2c599524c52c
Michael Naguib <1michael.naguib@gmail.com>
2019-11-06 20:48:42 -0600

    Finally solved the problem with the old codegit add view_sim.pygit add view_sim.pygit add view_sim.py Read the comment in the code to learn more; Solution: manually delete the open3d site package then reinstall in a conda admin cmd prompt and update the code to the latest lib; TL;DR Problem solved

--- view_sim.py
+++ view_sim.py
@@ -16,7 +16,10 @@ import math
 import copy
 import numpy as np
 import pickle
-import open3d
+from open3d import open3d # <-- Time Lost here: 3 WEEKS Why: weird installation procedure for open3d .... what worked: manually delete open3d in\
+# in the python site packages and reinstall using an admin conda command prompt ... also the docs i read for this were totally wack... but it worked on my old pc
+# idk why ... i must have installed a previous version where those docs were valid!
+
 import time
 print(" Finished Imports ... reading file")
 #Settings
@@ -25,22 +28,35 @@ timeDelay = 0.02 # 0.5=half second ... used to space out each frame in the playb
 #Read the file
 positionFrameHistory = pickle.load(open("position.dat","rb"))
 print("Finished Reading the file ... Starting Playback")
+
+HEIGHT = 600.0
+WIDTH = round(HEIGHT * 1.6180339) # preserve a nice aspect ratio by using phi= THE GOLDEN RATIO (one of my favorite numbers)
+DEPTH = HEIGHT
+
+
 #Setup 3D views
-pcd = open3d.PointCloud()
-vis = open3d.Visualizer()
+pcd = open3d.geometry.PointCloud()
+vis = open3d.visualization.Visualizer()
 vis.create_window()
 vis.add_geometry(pcd)
 
+#The following code adds a persistant wire box mesh around the sim ... 
+mesh_box = open3d.geometry.TriangleMesh.create_box(width=WIDTH,height=HEIGHT,depth=DEPTH)
+wireBox = open3d.geometry.LineSet.create_from_triangle_mesh(mesh_box)
+vis.add_geometry(wireBox)
+
 #Setup the initial frame
-pcd.points = open3d.Vector3dVector(positionFrameHistory[0])
-pcd.colors = open3d.Vector3dVector([[255,255,255] for i in range(0,len(positionFrameHistory[0]))])
+pcd.points = open3d.utility.Vector3dVector(positionFrameHistory[0])
+pcd.colors = open3d.utility.Vector3dVector([[0,0,0] for i in range(0,len(positionFrameHistory[0]))])
 
 render_option = vis.get_render_option()
 render_option.point_size = 5#0.01
 
+
+
 #precompute
 for i in range(1,len(positionFrameHistory)):
-    positionFrameHistory[i]=open3d.Vector3dVector(positionFrameHistory[i])
+    positionFrameHistory[i]=open3d.utility.Vector3dVector(positionFrameHistory[i])
 #render_option.point_color_option = open3d.visualization.PointColorOption.Normal
 to_reset_view_point = True
 for i in range(1,len(positionFrameHistory)):    
@@ -56,3 +72,4 @@ for i in range(1,len(positionFrameHistory)):
     time.sleep(timeDelay)# could be interpreted as frames per second assuming the calculation is instant and retrieval is as well
 
 vis.destroy_window()
+
