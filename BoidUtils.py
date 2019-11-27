1a14e1a65cc3da43a6b3c9df9fc8fb21fe15076b
Michael Naguib <1michael.naguib@gmail.com>
2019-11-26 21:23:32 -0600

    Found typo in the forces calculation; count was not incremented; TL;DR; Typo

--- BoidUtils.py
+++ BoidUtils.py
@@ -32,7 +32,6 @@ class BoidUtils:
         assert(s!=0)
         absoluteValues = [relValue*maxValue*(1/s) for relValue in relValues]
         return absoluteValues
-
     @staticmethod
     def cohereForce(boid,boids,neighbors,distances,distance,weight,dim=3):
         '''
@@ -46,6 +45,7 @@ class BoidUtils:
         :return:            vector representing the force update
         '''
         pos_sum = np.zeros(dim)
+        mass_sum=0
         count = 0
         # For every Neighbor:
         for indx in range(len(neighbors)):
@@ -53,15 +53,17 @@ class BoidUtils:
             if distances[indx]> distance:
                 continue# Goto the next
             # otherwise
-            pos_sum = np.add(pos_sum,boids[neighbors[indx]].pos*(1/boids[neighbors[indx]].mass))
+            mass = boids[neighbors[indx]].mass
+            mass_sum+=mass
+            pos_sum = np.add(pos_sum,boids[neighbors[indx]].pos*(1/mass))
+            count+=1
         # If there were no force updates return 0 vect
         if count==0:
             return pos_sum
         # otherwise
-        target = pos_sum*(1/count)
+        target = pos_sum*(1/mass_sum)
         steer = np.subtract(np.subtract(target,boid.pos),boid.vel)
         return steer*weight
-
     @staticmethod
     def seperateForce(boid,boids,neighbors,distances,distance,weight,dim=3):
         '''
@@ -84,10 +86,11 @@ class BoidUtils:
             # otherwise
             # calculate current separation distance direction and weight by edge distance
             edge_dist = distances[indx]
-            sep_norm = np.subtract(boid.pos,boids[neighbors[indx]].pos) * (1/math.pow(edge_dist if edge_dist!=0 else random.random(),2))# Catch divide by zero errors by inroducing a small random value
+            sep_norm = np.subtract(boid.pos,boids[neighbors[indx]].pos)* (1/(edge_dist if edge_dist!=0 else random.random()))# Catch divide by zero errors by inroducing a small random value
             # since the two boids overlap exactly ... introduce some small uncertianty in the distance ... interesting ... this makes sense from a practical programatic perspective...
-            # unvertianty in the location of an item at small scales makes sense ... Heisenburg uncertianty?
-            pos_sum = np.subtract(pos_sum,sep_norm*boids[neighbors[indx]].mass)
+            # uncertainty in the location of an item at small scales makes sense ... Heisenburg uncertianty?
+            pos_sum = np.subtract(pos_sum,sep_norm)#*boids[neighbors[indx]].mass)
+            count+=1
 
         # If there were no force updates return 0 vect
         if count==0:
@@ -119,214 +122,12 @@ class BoidUtils:
             # otherwise
             mass = boids[neighbors[indx]].mass
             mass_sum+=mass
-            vel_sum = np.add(vel_sum,boids[indx].vel)*mass
+            vel_sum = np.add(vel_sum,boids[indx].vel*mass)
+            count+=1
         # If there were no force updates return 0 vect
         if count==0:
             return vel_sum
         # otherwise
-        target = vel_sum*(1/(count*mass_sum))
-        steer = np.add(target,boid.vel)
+        target = vel_sum*(1/(mass_sum))
+        steer = np.subtract(target,boid.vel)
         return steer*weight
-    #=== Predefined Update rules! these may be used withine the BoidSwarm ... custom rules can be added...
-    @staticmethod
-    def Cohere(boid,boid_list,distance_map,weight,ranger,max_neighbor=10):
-        '''
-        :description: calculates the weighted coherernce force for a given boid based upon the boid list and distance map etc...
-        :param boid: the current boid for which the calculation is being done....
-        :param boid_list: a list containing all of the boids
-        :param distance_map: a dictionary of key=index, value=distance for every boid in the boid_list
-        :param weight:  the weight for this rule
-        :param range:   the range for which this rule applies
-        :return: the force vector...
-        '''
-        # store
-        pos_sum = Vector.zeros(dim=boid.pos.shape())
-        count = 0
-
-        #iterate over the distances in the map...
-        for key in distance_map:
-            # Note for future use: if cohesion is a per swarm property that is to say that cohesion exists differently (relative varying ammounts)
-            # or not at all between swarms in the latter case the algorithm could be sped up by partitioning the swarm space so it does not have t
-            # to iterate over known partitions for which it does not interact....
-            edge_dist = distance_map[key]
-            # compare the edge distance to influence radius of the current boid
-            if 0<edge_dist and edge_dist <= ranger:#  if the edge dist = 0 ==> it is itself so exclude....
-                count += 1
-                pos_sum = pos_sum + boid_list[key].pos#sum the positions
-            if  bool(max_neighbor) and count >=max_neighbor:
-                break
-
-        #Determine the steering:
-        if count >0:
-            target = pos_sum * (1/count)
-            desired = (target - boid.pos).to_len(boid.max_vel)
-            steer = desired - boid.vel
-            return steer.limit(boid.max_force)*weight
-        else:
-            return pos_sum# will be Vector([0,0])
-    @staticmethod
-    def Separate(boid,boid_list,distance_map,weight,ranger,max_neighbor=10):
-        '''
-        :description: calculates the weighted separation force for a given boid based upon the boid list and distance map etc...
-        :param boid: the current boid for which the calculation is being done....
-        :param boid_list: a list containing all of the boids
-        :param distance_map: a dictionary of key=index, value=distance for every boid in the boid_list
-        :param weight:  the weight for this rule
-        :param range:   the range for which this rule applies
-        :return: the force vector...
-        '''
-        # store
-        pos_sum = Vector.zeros(dim=boid.pos.shape())
-        count = 0
-
-        #iterate over the distances in the map...
-        for key in distance_map:
-            # Note for future use: if cohesion is a per swarm property that is to say that cohesion exists differently (relative varying ammounts)
-            # or not at all between swarms in the latter case the algorithm could be sped up by partitioning the swarm space so it does not have t
-            # to iterate over known partitions for which it does not interact....
-            edge_dist = distance_map[key]
-            # compare the edge distance to influence radius of the current boid
-            if 0<edge_dist and edge_dist <= ranger:#  if the edge dist = 0 ==> it is itself so exclude....
-                count += 1
-                #calculate current separation distance direction and weight by edge distance
-                pos_sum = pos_sum + ((boid.pos-boid_list[key].pos).normalized()* (1/edge_dist))
-            if  bool(max_neighbor) and count >=max_neighbor:
-                break
-
-        #Now convert to force...
-        if count>0:
-            pos_sum = pos_sum * (1/count)#convert it to an average
-            steer = (pos_sum.to_len(boid.max_vel)-boid.vel).limit(boid.max_force)# Steering
-            return steer*weight
-        else:
-            return Vector.zeros(dim=boid.pos.shape())
-    @staticmethod
-    def  Align(boid,boid_list,distance_map,weight,ranger,max_neighbor=10):
-        '''
-        :description: calculates the weighted alignment force for a given boid based upon the boid list and distance map etc...
-        :param boid: the current boid for which the calculation is being done....
-        :param boid_list: a list containing all of the boids
-        :param distance_map: a dictionary of key=index, value=distance for every boid in the boid_list
-        :param weight:  the weight for this rule
-        :param range:   the range for which this rule applies
-        :return: the force vector...
-        '''
-        # store
-        vel_sum = Vector.zeros(dim=boid.vel.shape())
-        count = 0
-
-        #iterate over the distances in the map...
-        for key in distance_map:
-            # Note for future use: if cohesion is a per swarm property that is to say that cohesion exists differently (relative varying ammounts)
-            # or not at all between swarms in the latter case the algorithm could be sped up by partitioning the swarm space so it does not have t
-            # to iterate over known partitions for which it does not interact....
-            edge_dist = distance_map[key]
-            # compare the edge distance to influence radius of the current boid
-            if 0<edge_dist and edge_dist <= ranger:#  if the edge dist = 0 ==> it is itself so exclude....
-                count += 1
-                #calculate current separation distance direction and weight by edge distance
-                vel_sum = vel_sum + boid_list[key].vel
-            if  bool(max_neighbor) and count >=max_neighbor:
-                break
-        #Now convert to force...
-        if count>0:
-            vel_sum = vel_sum * (1/count)#convert it to an average
-            steer = (vel_sum.to_len(boid.max_vel)+boid.vel).limit(boid.max_force)# Steering
-            return steer*weight
-        else:
-            return Vector.zeros(dim=boid.pos.shape())
-    #avoid boids in front view... turn...
-    #=== Useful methods
-    @staticmethod
-    def euclidian_distance(pos_0,pos_1,modular_space=False,squared=False):
-        '''
-
-        :param pos_0: a position of type Vector
-        :param pos_1: a positionof type Vector
-        :param modular_space:  defaults to false ==> computes euclidian distance assuming the topography 'wraps' ...
-                               pass a Vector containing the maximmum allowed values
-                               where it is assumed the world is bounded by min_x=0,min_y=0 ...etc... and where
-                               y_max and x_max are the maximum allowed x and y_positions within the world etc....
-        :param squared: defaults to false ==> as sqrt is a expensive operation when computing length ... you can
-                        just get the distance squared easily and do a multiplication of the distance you want to compare
-                        to and that is more efficient...
-        :return: distance as float
-        '''
-        if not modular_space:
-            #Normal implemetation of euclidian distance
-            return (pos_0-pos_1).mag() if not squared else sum((pos_0-pos_1).apply(lambda x: x*x).components)
-        else:
-            #use the equation i derived for modular space euclidian distance   min(|x1-x2|,max_x - |x1-x2|)   same applies to y (note the order is not specific for x2-x1 or x1-x2 but it must be consistant
-
-            #Compte the seperation of the components
-            delta = (pos_0-pos_1).apply(abs)
-            #compute distance from boundry
-            mod_dist  = modular_space - delta
-            #compute the minumum distance for each... ==> distance components
-            dist_vect = Vector.comp(min,delta,mod_dist)
-            #returns the distance between the two n dimensional points in modular space
-            return dist_vect.mag() if not squared else sum(dist_vect.apply(lambda x:x*x).components)
-    @staticmethod#@timeit
-    #@deprecated(reason="This is n^2 time complexity... use calc_distance_mapV2 as it has a time complexity of 1/2 n^2 + 1/2 n")
-    def calc_distance_map(list_of_positions, modular_space=False,squared=False):#Using timeit on this determined that this was the slowest part of thecode ...
-        '''
-        :description: uses a caching based approach to calculate a dictionaries of dictionaries containing the position distances:
-                          A   to    B         dist
-                      {index:{ otherindex: distance,....},....}
-        :param list_of_positions: a list of position Vectors [Vector,Vector,...,Vector]
-        :modular_space: see euclidian_distance docstring
-        squared: see euclidian_distance docstring
-        :return: (see description)
-        '''
-        cache={}# really only gets a slim asyntotic advantage...
-        for i in range(0,len(list_of_positions)):
-            #Determine if the current entry is empty if so put a new array else preserve it
-            if i not in cache:
-                cache[i] = {}
-            #now we need to go through all the positions
-            for j in range(0,len(list_of_positions)):
-                #if a positon edge is not already in the cache we need to compute it
-                if j not in cache[i]:
-                    #Run the 'intensive' calculation
-                    dist = BoidUtils.euclidian_distance(list_of_positions[i],list_of_positions[j],modular_space=modular_space,squared=squared)
-                    #Now we add it to the two locations it will be A-B and B-A    --> cache[i][j] and cache[j][i]
-                    cache[i][j] = dist# we know cache[i] exists so add it to that dictionary
-                    #we are however not sure if cache[j] exists yet and if it does not we need to init it first (rather if there is a dictionary at key j)
-                    if j not in cache:
-                        cache[j]={}
-                    #now we can add the distance...
-                    cache[j][i] = dist
-                else:
-                    pass # it was added to both dictionaries above...
-        #(str(c)+" " + str(len(list_of_positions)*len(list_of_positions)))
-        return cache
-    @staticmethod#@timeit
-    def calc_distance_mapV2(list_of_positions, modular_space=False,squared=False):
-        #Using timeit on this determined that this was the slowest part of thecode ... V2 is the realization that
-        #the first distance map while efficient ... was still technically O(n^2) not the more desired O([1/2]n^2 + [1/2]n)
-        #this code fixes that...
-        '''
-        :description: uses a caching based approach to calculate a dictionaries of dictionaries containing the position distances:
-                          A   to    B         dist
-                      {index:{ otherindex: distance,....},....}
-        :param list_of_positions: a list of position Vectors [Vector,Vector,...,Vector]
-        :modular_space: see euclidian_distance docstring (note this may slow down by up to a factor of 4!!!
-        squared: see euclidian_distance docstring
-        :return: (see description)
-        '''
-        cache={}# really only gets a slim asyntotic advantage...
-        for i in range(0,len(list_of_positions)):
-            #Determine if the current entry is empty if so put a new array else preserve it
-            if i not in cache:# at a certian point i takes on the value that j was ... so to prevent overriding
-                cache[i] = {}
-            #now we need to go through all the positions
-            for j in range(len(list_of_positions)-1,i,-1):
-                dist = BoidUtils.euclidian_distance(list_of_positions[i],list_of_positions[j],modular_space=modular_space,squared=squared)
-                #Now we add it to the two locations it will be A-B and B-A    --> cache[i][j] and cache[j][i]
-                cache[i][j] = dist# we know cache[i] exists so add it to that dictionary
-                #we are however not sure if cache[j] exists yet and if it does not we need to init it first (rather if there is a dictionary at key j)
-                if j not in cache:#we need to have a dictionary there to put a value... make sure not to overwrite it...
-                    cache[j]={}
-                #now we can add the distance...
-                cache[j][i] = dist
-        return cache
